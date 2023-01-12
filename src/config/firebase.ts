32328a99c50debffc221fdf864d5cdf8100616ca
pupe.sirithanakij <pupe.sirithanakij@gmail.com>
2023-01-13 04:16:16 +0700

    remove unused code

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,7 +1,6 @@
-// import { initializeApp } from 'firebase-admin/app';
-import { getFirestore } from "firebase-admin/firestore";
-var admin = require("firebase-admin");
-const serviceAccount = {
+import admin from "firebase-admin";
+
+const serviceAccount: any = {
   "type": "service_account",
   "project_id": "trip-tac-tone",
   "private_key_id": "00c296f85a21993fa335c06352e29f144b0fc888",
@@ -14,29 +13,12 @@ const serviceAccount = {
   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-4wmcy%40trip-tac-tone.iam.gserviceaccount.com"
 }
 
-
-
-// const firebaseConfig = {
-//     apiKey: "AIzaSyC5Qp9fXxiQG4mLmNqCwbFA7ryN55cMMU8",
-//     authDomain: "trip-tac-tone.firebaseapp.com",
-//     databaseURL: "https://trip-tac-tone-default-rtdb.asia-southeast1.firebasedatabase.app",
-//     projectId: "trip-tac-tone",
-//     storageBucket: "trip-tac-tone.appspot.com",
-//     messagingSenderId: "131124707681",
-//     appId: "1:131124707681:web:67319854fc7105b3f88520"
-//   };
-  
-
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://trip-tac-tone-default-rtdb.asia-southeast1.firebasedatabase.app"
 });
 
-
-// const app=initializeApp(firebaseConfig);
-// const db = getFirestore(app);
-const db=admin.firestore();
+const db = admin.firestore();
 const cafesCollection = db.collection('Cafes');
 
-export { db}
+export { db, cafesCollection }
